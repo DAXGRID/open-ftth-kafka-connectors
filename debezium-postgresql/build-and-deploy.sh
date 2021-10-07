649f7f020ce326b1b716f7f10d73f442cc2e5589
Rune Nielsen <runenielsen@runbox.com>
2021-10-07 10:42:40 +0200

    upgrades postgres connector to 1.7.0 final

--- debezium-postgresql/build-and-deploy.sh
+++ debezium-postgresql/build-and-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-curl https://repo1.maven.org/maven2/io/debezium/debezium-connector-postgres/1.1.2.Final/debezium-connector-postgres-1.1.2.Final-plugin.tar.gz | tar xvz
+curl https://repo1.maven.org/maven2/io/debezium/debezium-connector-postgres/1.7.0.Final/debezium-connector-postgres-1.7.0.Final-plugin.tar.gz | tar xvz
 
 export DOCKER_ORG=openftth
-docker build . -t ${DOCKER_ORG}/connect-debezium-postgresql
-docker push ${DOCKER_ORG}/connect-debezium-postgresql
+docker build . -t ${DOCKER_ORG}/connect-debezium-postgresql:1.7.0.Final
+docker push ${DOCKER_ORG}/connect-debezium-postgresql:1.7.0.Final
