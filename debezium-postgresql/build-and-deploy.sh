8d17bf2f7d7d2286454ed0e05f6372e7dd4b2f99
Rune Nielsen <runenielsen@runbox.com>
2021-10-08 15:44:52 +0200

    set 1.7.0 final v2

--- debezium-postgresql/build-and-deploy.sh
+++ debezium-postgresql/build-and-deploy.sh
@@ -3,5 +3,5 @@
 curl https://repo1.maven.org/maven2/io/debezium/debezium-connector-postgres/1.7.0.Final/debezium-connector-postgres-1.7.0.Final-plugin.tar.gz | tar xvz
 
 export DOCKER_ORG=openftth
-docker build . -t ${DOCKER_ORG}/connect-debezium-postgresql:1.7.0.Final
-docker push ${DOCKER_ORG}/connect-debezium-postgresql:1.7.0.Final
+docker build . -t ${DOCKER_ORG}/connect-debezium-postgresql:1.7.0.Final.v2
+docker push ${DOCKER_ORG}/connect-debezium-postgresql:1.7.0.Final.v2
